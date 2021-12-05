fc53ba4f650d12618bf3b66fc06775c29de934b6
MaximBoolippe <babich.development@gmail.com>
2021-12-05 14:17:58 +0300

    Плюс тюплы в комментах

--- Just Tests.py
+++ Just Tests.py
@@ -1,18 +1,20 @@
 #Прикол с хуесосом name = input('Введите имя: ') print('Привет,', name,',ты хуесос' )
 
-#Прога меняет местами первые буквы слов name1 =input('Введите имя: ')
+#Прога меняет местами первые буквы слов
+# name1 =input('Введите имя: ')
 # a=name1[0]
-# b=name1[1:-1]
-# c=name1[-1]
+# b=name1[1:]
 # name2 = input('введите имя: ')
 # d=name2[0]
-# e=name2[1:-1]
-# f=name2[-1]
-# name4=print(d+b+c, a+e+f)
+# e=name2[1:]
+# name4=print(d+b, a+e)
 
 
-# Хуйня с координатами x, y = map(int, input() .split())
+#Хуйня с координатами x, y = map(int, input() .split())
 # print(type(x))
 # print(x, y)
+
+# user = tuple(["Tom", 23])
+# print(user)
 
 
