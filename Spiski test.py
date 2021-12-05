3faa99590e57be71f5f969c57bc2f26d9a023f9a
MaximBoolippe <babich.development@gmail.com>
2021-12-05 20:35:01 +0300

    Плюс тюплы в комментах

---
+++ Spiski test.py
@@ -0,0 +1,11 @@
+companies = ["Microsoft", "Google", "Oracle", "Apple"]
+for item in companies:
+    print(item)
+
+
+
+companies = ["Microsoft", "Google", "Oracle", "Apple", "Amazon"]
+i = 0
+while i < len(companies):
+    print(companies[i])
+    i=i+1
