df00485ab3cdea39ff1f32375814843c7c2a6783
MaximBoolippe <babich.development@gmail.com>
2021-12-05 21:18:47 +0300

    Программа про слова

---
+++ Hyy.py
@@ -0,0 +1,10 @@
+name1 =input('Введите имя: ')
+name2=name1[0]
+name3=name1[1:-1]
+name4=name1[-1]
+namee1 = input('введите имя: ')
+namee2=namee1[0]
+namee3=namee1[1:-1]
+namee4=namee1[-1]
+name4=print(namee2+name3+name4, name2+namee3+namee4)
+k=input("press close to exit")
